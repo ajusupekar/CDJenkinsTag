9d952cfa27af60df25b903d6a1482df0a3361fbc
Ajinkya <ajinkya.supekar@bnt-soft.com>
2022-08-30 13:46:40 +0530

    ss

--- CDSIT-MobAutomation-Project/src/test/java/TestNGRunner.java
+++ CDSIT-MobAutomation-Project/src/test/java/TestNGRunner.java
@@ -13,7 +13,7 @@ import cucumber.api.CucumberOptions;
 		glue = { "com.cucumber.stepdefinition" }, strict = false, dryRun =false,
 
 
-		 tags = { "@PAD-3040" },
+		 tags = { "@PAD-2487_TC19" },
 
 
 		plugin = { "pretty", "html:target/site/cucumber-pretty",
